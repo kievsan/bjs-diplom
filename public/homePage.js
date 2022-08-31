cdfafb3383e33edfc004ffe2af36e90ef40cf1c5
Sergey Kievskiy <knhel7@mail.ru>
2022-08-31 05:14:07 +0300

    Edit loginPage.js, second ed., homepage.js first ed.

--- public/homePage.js
+++ public/homePage.js
@@ -2,10 +2,24 @@
 
 // Личный кабинет пользователя
 
+// Получение информации о пользователе
+ApiConnector.current((response) => response.success ? ProfileWidget.showProfile(response.data) : {});
+
+// Получение текущих курсов валюты
+const exchangeRates = new RatesBoard();
+exchangeRates.intervalID = setInterval( () =>
+    ApiConnector.getStocks((response) => {
+        if (response.success) {
+            exchangeRates.clearTable();
+            exchangeRates.fillTable(response.data);
+        }
+    }), 1000);
+
 // Выход из личного кабинета
 const homeExit = new LogoutButton();
-homeExit.action = (data) => ApiConnector.logout((response) => (response.success ? location.reload() : {}));
-
-// Получение информации о пользователе
-ApiConnector.current((response) => (response.success ? ProfileWidget.showProfile(response.data) : {}));
-
+homeExit.action = (data) => ApiConnector.logout((response) => {
+    if (response.success) {
+        clearInterval(exchangeRates.intervalID);
+        location.reload();
+    }
+});
