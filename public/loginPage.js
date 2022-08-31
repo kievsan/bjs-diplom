2f6c54512da1a27158dc1b2b67c7b659396e3180
Sergey Kievskiy <knhel7@mail.ru>
2022-08-31 13:50:12 +0300

    Edit loginpage.js third ed. homepage.js first ed.

--- public/loginPage.js
+++ public/loginPage.js
@@ -6,5 +6,5 @@
 const userForm = new UserForm();
 userForm.loginFormCallback = (data) => ApiConnector.login(data, (response) =>
     response.success ? location.reload() : userForm.setLoginErrorMessage(response.error));
-userForm.registerFormCallback = (data) => ApiConnector.login(data, (response) =>
+userForm.registerFormCallback = (data) => ApiConnector.register(data, (response) =>
     response.success ? location.reload() : userForm.setRegisterErrorMessage(response.error));
